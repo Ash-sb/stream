6b73f690ca01414111752e6d653cf91ead39353f
Diane Sales <diane.sales@hetic.net>
2021-05-05 09:57:23 +0200

    index and live changes

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="asset\style\style.css">
+  <link rel="stylesheet" href="../stream/asset/style/style.css">
   <title>Document</title>
 </head>
 </header>
@@ -13,70 +13,70 @@
   <section>
   <!--Création du bloc de gauche : Création d'un groupe et canaux de discussions -->
     <p>Créer un groupe</p>
-    <p>Discussion - 5<img class="#", src="asset\plus-square.svg" , alt="icone1"></p>
+    <p>Discussion - 5<img class="#", src="../stream/asset/img/plus-square.svg" , alt="icone1"></p>
     <div class="membre">
       <div>
-        <img class="home-photo", src="asset\avatar.jpg" , alt="profil1">
+        <img class="home-photo", src="../stream/asset/img/avatar.jpg" , alt="profil1">
       </div>
       <div class="home-cartouche">
         <p>Prénom</p>
         <p>Nom</p>
       </div>
       <div class="home-cartouche">
-        <img class="#", src="asset\message-circle.svg" , alt="icone1">
-        <img class="#", src="asset\video.svg" , alt="icone2">
+        <img class="#", src="../stream/asset/img/message-circle.svg" , alt="icone1">
+        <img class="#", src="../stream/asset/img/video.svg" , alt="icone2">
       </div>
     </div>
     <div class="membre">
       <div>
-        <img class="home-photo", src="asset\avatar.jpg" , alt="profil1">
+        <img class="home-photo", src="../stream/asset/img/avatar.jpg" , alt="profil1">
       </div>
       <div class="home-cartouche">
         <p>Prénom</p>
         <p>Nom</p>
       </div>
       <div class="home-cartouche">
-        <img class="#", src="asset\message-circle.svg" , alt="icone1">
-        <img class="#", src="asset\video.svg" , alt="icone2">
+        <img class="#", src="../stream/asset/img/message-circle.svg" , alt="icone1">
+        <img class="#", src="../stream/asset/img/video.svg" , alt="icone2">
       </div>
     </div>
     <div class="membre">
       <div>
-        <img class="home-photo", src="asset\avatar.jpg" , alt="profil1">
+      <img class="home-photo", src="../stream/asset/img/avatar.jpg" , alt="profil1">
       </div>
       <div class="home-cartouche">
         <p>Prénom</p>
         <p>Nom</p>
       </div>
       <div class="home-cartouche">
-        <img class="#", src="asset\message-circle.svg" , alt="icone1">
-        <img class="#", src="asset\video.svg" , alt="icone2">
+        <img class="#", src="../stream/asset/img/message-circle.svg" , alt="icone1">
+        <img class="#", src="../stream/asset/img/video.svg" , alt="icone2">
       </div>
     </div>
     <div class="membre">
       <div>
-        <img class="home-photo", src="asset\avatar.jpg" , alt="profil1">
+      <img class="home-photo", src="../stream/asset/img/avatar.jpg" , alt="profil1">
       </div>
       <div class="home-cartouche">
         <p>Prénom</p>
         <p>Nom</p>
       </div>
       <div class="home-cartouche">
-        <img class="#", src="asset\message-circle.svg" , alt="icone1">
-        <img class="#", src="asset\video.svg" , alt="icone2">
+        <img class="#", src="../stream/asset/img/message-circle.svg" , alt="icone1">
+        <img class="#", src="../stream/asset/img/video.svg" , alt="icone2">
       </div>
     </div>
   </section>
   <section class="home-chat">
     <div class="home-en-tete">
       <p>Chat</p>
-      <img class="home-icone", src="asset\user-plus.svg" , alt="icone1">
-      <img class="home-icone", src="asset\video.svg" , alt="icone2">
-      <img class="home-icone", src="asset\image.svg" , alt="icone3">
+      <img class="home-icone", src="../stream/asset/img/user-plus.svg" , alt="icone1">
+      <img class="home-icone", src="../stream/asset/img/video.svg" , alt="icone2">
+      <img class="home-icone", src="../stream/asset/img/image.svg" , alt="icone3">
     </div>
     <div class="home-message-impair" >
       <div>
-        <img class="home-photo", src="asset\avatar.jpg" , alt="profil2">
+      <img class="home-photo", src="../stream/asset/img/avatar.jpg" , alt="profil2">
       </div>
       <div>
         <div class="home-message2">
@@ -87,7 +87,7 @@
     </div>
     <div class="home-message-pair">
       <div>
-        <img class="home-photo", src="asset\avatar.jpg" , alt="profil2">
+      <img class="home-photo", src="../stream/asset/img/avatar.jpg" , alt="profil2">
       </div>
       <div>
         <div class="home-message2">
@@ -98,7 +98,7 @@
     </div>
     <div class="home-message-impair">
       <div>
-        <img class="home-photo", src="asset\avatar.jpg" , alt="profil2">
+      <img class="home-photo", src="../stream/asset/img/avatar.jpg" , alt="profil2">
       </div>
       <div>
         <div class="home-message2">
@@ -109,7 +109,7 @@
     </div>
     <div class="home-message-pair">
       <div>
-        <img class="home-photo", src="asset\avatar.jpg" , alt="profil2">
+      <img class="home-photo", src="../stream/asset/img/avatar.jpg" , alt="profil2">
       </div>
       <div>
         <div class="home-message2">
@@ -130,54 +130,54 @@
     <p>Vos amis</p>
     <div class="membre">
       <div>
-        <img class="home-photo", src="asset\avatar.jpg" , alt="profil1">
+      <img class="home-photo", src="../stream/asset/img/avatar.jpg" , alt="profil1">
       </div>
       <div class="home-cartouche">
         <p>Prénom</p>
         <p>Nom</p>
       </div>
       <div class="home-cartouche">
-        <img class="#", src="asset\message-circle.svg" , alt="icone1">
-        <img class="#", src="asset\video.svg" , alt="icone2">
+        <img class="#", src="../stream/asset/img/message-circle.svg" , alt="icone1">
+        <img class="#", src="../stream/asset/img/video.svg" , alt="icone2">
       </div>
     </div>
     <div class="membre">
       <div>
-        <img class="home-photo", src="asset\avatar.jpg" , alt="profil1">
+        <img class="home-photo", src="../stream/asset/img/avatar.jpg" , alt="profil1">
       </div>
       <div class="home-cartouche">
         <p>Prénom</p>
         <p>Nom</p>
       </div>
       <div class="home-cartouche">
-        <img class="#", src="asset\message-circle.svg" , alt="icone1">
-        <img class="#", src="asset\video.svg" , alt="icone2">
+        <img class="#", src="../stream/asset/img/message-circle.svg" , alt="icone1">
+        <img class="#", src="../stream/asset/img/video.svg" , alt="icone2">
       </div>
     </div>
     <div class="membre">
       <div>
-        <img class="home-photo", src="asset\avatar.jpg" , alt="profil1">
+        <img class="home-photo", src="../stream/asset/img/avatar.jpg" , alt="profil1">
       </div>
       <div class="home-cartouche">
         <p>Prénom</p>
         <p>Nom</p>
       </div>
       <div class="home-cartouche">
-        <img class="#", src="asset\message-circle.svg" , alt="icone1">
-        <img class="#", src="asset\video.svg" , alt="icone2">
+        <img class="#", src="../stream/asset/img/message-circle.svg" , alt="icone1">
+        <img class="#", src="../stream/asset/img/video.svg" , alt="icone2">
       </div>
     </div>
     <div class="membre">
       <div>
-        <img class="home-photo", src="asset\avatar.jpg" , alt="profil1">
+        <img class="home-photo", src="../stream/asset/img/avatar.jpg" , alt="profil1">
       </div>
       <div class="home-cartouche">
         <p>Prénom</p>
         <p>Nom</p>
       </div>
       <div class="home-cartouche">
-        <img class="#", src="asset\message-circle.svg" , alt="icone1">
-        <img class="#", src="asset\video.svg" , alt="icone2">
+        <img class="#", src="../stream/asset/img/message-circle.svg" , alt="icone1">
+        <img class="#", src="../stream/asset/img/video.svg" , alt="icone2">
       </div>
     </div>
   </section>
